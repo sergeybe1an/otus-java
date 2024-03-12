4162db7b2f57961171af637d340131985fac62d4
sbelan <sergey.belan@novardis.com>
2024-03-12 22:13:44 +0300

    [hw16] changed resource file loader to impl through getResourceAsStream

--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -3,12 +3,9 @@ package ru.otus.dataprocessor;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 import org.slf4j.Logger;
@@ -19,15 +16,15 @@ public class ResourcesFileLoader implements Loader {
 
     private final static Logger logger = LoggerFactory.getLogger(ResourcesFileLoader.class);
     private final ObjectMapper mapper = new ObjectMapper();
-    private final File inputFile;
+    private final String fileName;
 
     public ResourcesFileLoader(String fileName) {
-        this.inputFile = getFile(fileName);
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-        try (var bufferedReader = new BufferedReader(new FileReader(inputFile))) {
+        try (var bufferedReader = new BufferedReader(new InputStreamReader(getFileAsIS(fileName)))) {
             StringBuilder sb = new StringBuilder();
 
             String line;
@@ -35,24 +32,15 @@ public class ResourcesFileLoader implements Loader {
                 sb.append(line);
             }
 
-            logger.info("{} file loaded - {}", inputFile.getPath(), sb);
+            logger.info("{} file loaded - {}", fileName, sb);
             return mapper.readValue(sb.toString(), new TypeReference<>() {});
         } catch (IOException ioe) {
-            logger.error("Error while reading file {} - {}!", inputFile.getPath(), ioe.getMessage());
+            logger.error("Error while reading file {} - {}!", fileName, ioe.getMessage());
             return Collections.emptyList();
         }
     }
 
-    private File getFile(String fileName) {
-        try {
-            URL url = this.getClass().getClassLoader().getResource(fileName);
-
-            if (url == null) throw new URISyntaxException(fileName, "URL is null!");
-
-            return new File(new URI(url.toString()));
-        } catch (URISyntaxException e) {
-            logger.error("Wrong file name {}!", fileName);
-            throw new RuntimeException(e);
-        }
+    private InputStream getFileAsIS(String fileName) {
+        return this.getClass().getClassLoader().getResourceAsStream(fileName);
     }
 }
